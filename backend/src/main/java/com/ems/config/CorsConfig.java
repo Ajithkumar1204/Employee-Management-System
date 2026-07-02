a2ceb4c4ec035daf0e96ce764f66be451b696206
Ajithkumar <ajithkumar1204@gmail.com>
2026-07-02 16:41:22 +0530

    solving final 403 problem

--- backend/src/main/java/com/ems/config/CorsConfig.java
+++ backend/src/main/java/com/ems/config/CorsConfig.java
@@ -25,7 +25,7 @@ public class CorsConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:5173",
                 "http://localhost:3000",
-                "https://your-vercel-app.vercel.app"  // Replace with your Vercel URL
+                "https://employee-management-systemz.netlify.app"  // Replace with your netlify URL
         ));
 
         // Allow all standard HTTP methods
